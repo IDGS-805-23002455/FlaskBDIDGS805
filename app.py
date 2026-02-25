c0975818e4567cc34ce399d732cd5bf3124f51fd
unknown <83168@alumnos.utelon.edu.mx>
2026-02-24 23:51:04 -0600

    Migraciones 24/02/2026

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask import request, redirect, url_for, flash
 from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate
 from config import DevelopmentConfig
 import forms
 from models import db, Alumnos
@@ -8,6 +9,7 @@ from models import db, Alumnos
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 db.init_app(app)
+migrate=Migrate(app, db)
 csrf = CSRFProtect()
 
 @app.errorhandler(404) 
@@ -25,31 +27,74 @@ def index():
 def alumnos():
 	create_form=forms.UserForm(request.form)
 	if request.method == 'POST':
-		alum =Alumnos(nombre=create_form.nombre.data,
-					  apaterno=create_form.apaterno.data,
-					  email=create_form.email.data,)
+		alum = Alumnos(
+            nombre=create_form.nombre.data,
+            apellidos=create_form.apellidos.data,  # <-- CAMBIA ESTO
+            email=create_form.email.data,
+            telefono=create_form.telefono.data     # <-- AGREGA ESTO
+        )
 		db.session.add(alum)
 		db.session.commit()
 		return redirect(url_for('index'))
 	return render_template("Alumnos.html", form=create_form)
 
-@app.route('/detalles', methods=['GET'])
+@app.route("/detalles", methods=['GET','POST'])
 def detalles():
     id = request.args.get('id')
     alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
-
-    if alum1:
-        return render_template(
-            'detalles.html',
-            id=alum1.id,
-            nombre=alum1.nombre,
-            apaterno=alum1.apaterno,
-            email=alum1.email
-        )
-    else:
+    
+    nombre = alum1.nombre
+    apellidos = alum1.apellidos
+    email = alum1.email
+    telefono = alum1.telefono
+    return render_template('detalles.html', id=id, nombre=nombre, 
+                           apellidos=apellidos, email=email, telefono=telefono)
+    
+@app.route('/modificar', methods=['GET', 'POST'])
+def modificar():
+    create_form = forms.UserForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        create_form.id.data = alum1.id
+        create_form.nombre.data = alum1.nombre
+        create_form.apellidos.data = alum1.apellidos
+        create_form.email.data = alum1.email
+        create_form.telefono.data = alum1.telefono 
+    if request.method == 'POST':
+        id = create_form.id.raw_data
+        alum = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        alum.nombre = create_form.nombre.data
+        alum.apellidos = create_form.apellidos.data
+        alum.email = create_form.email.data
+        alum.telefono = create_form.telefono.data
+        db.session.add(alum)
+        db.session.commit()
         return redirect(url_for('index'))
+    return render_template("modificar.html", form=create_form)
+
+@app.route('/eliminar', methods=['GET', 'POST'])
+def eliminar():
+    create_form = forms.UserForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        if alum1:
+            create_form.id.data = alum1.id
+            create_form.nombre.data = alum1.nombre
+            create_form.apellidos.data = alum1.apellidos
+            create_form.email.data = alum1.email
+            create_form.telefono.data = alum1.telefono
+    if request.method == 'POST':
+        id = create_form.id.data
+        alum = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        if alum:
+            db.session.delete(alum)
+            db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("eliminar.html", form=create_form)
 
 if __name__ == '__main__':
-	with app.app_context():
-		db.create_all()
-	app.run()
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -3,23 +3,30 @@ from wtforms import IntegerField,StringField,PasswordField
 from wtforms import EmailField
 from wtforms import validators
 
-
 class UserForm(Form):
-    id=IntegerField("id",[
+    id = IntegerField("id", [
         validators.DataRequired(message="El campo es requerido"),
-        validators.NumberRange(min=100, max=1000, message="Ingrese valor valido")
-    ])
-    nombre=StringField("Nombre",[
-         validators.DataRequired(message="El campo es requerido"),
-         validators.length(min=4, max=10, message="Ingrese nombre valido")
-    ])
-    apaterno=StringField("Apaterno", [
-        validators.DataRequired(message="El campo es requerido"),
-        validators.length(min=4, max=10, message="Ingrese apellido valido")
-
+        validators.NumberRange(min=1, max=10000, message="Ingrese valor valido")
     ])
     
-    email =EmailField("Correo", [
+    nombre = StringField("Nombre", [
+        validators.DataRequired(message="El campo es requerido"),
+        validators.length(min=4, max=20, message="Ingrese nombre valido")
+    ])
+
+    # Se cambió 'apaterno' por 'apellidos' para coincidir con tu HTML
+    apellidos = StringField("Apellidos", [
+        validators.DataRequired(message="El campo es requerido"),
+        validators.length(min=4, max=50, message="Ingrese apellidos válidos")
+    ])
+    
+    email = EmailField("Correo", [
         validators.DataRequired(message="El campo es requerido"),
         validators.Email(message="Ingrese un correo valido")
+    ])
+
+    # Nuevo campo de teléfono añadido
+    telefono = StringField("Teléfono", [
+        validators.DataRequired(message="El campo es requerido"),
+        validators.length(min=10, max=15, message="Ingrese un teléfono válido")
     ])
